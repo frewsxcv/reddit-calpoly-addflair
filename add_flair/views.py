b8a58043221e43945f282e074730c26799834a18
Corey Farwell <coreyf@rwell.org>
2011-08-23 23:52:52 -0700

    made more functions, looks nicer

--- add_flair/views.py
+++ add_flair/views.py
@@ -10,26 +10,35 @@ def add(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data['username']
-            year = form.cleaned_data['year']
-            major = form.cleaned_data['major']
             confirm_num = generate_confirm_num()
-            message = "Send this message to confirm your /r/CalPoly flair.\n"
-            message += "Confirmation number: " + str(confirm_num)
-            args = (("to", "rCalPolyBot"), ("subject", "Add flair"),
-                    ("message", message))
-            compose = 'http://reddit.com/message/compose/?'
-            user = form.save(commit=False)
-            user.confirmed = False
-            user.confirm_num = confirm_num
-            user.save()
-            return redirect(compose + urlencode(args))
+            save_user(form, confirm_num)
+            return redirect(gen_url(form, confirm_num))
     else:
         form = UserForm()
     return render_to_response('add.html', {
         'form': form,
         'csrf_token': csrf(request)['csrf_token']
     })
+
+
+def gen_url(form, confirm_num):
+    """ Generate a reddit compose URL from a form object """
+    username = form.cleaned_data['username']
+    year = form.cleaned_data['year']
+    major = form.cleaned_data['major']
+    msg = "Send this message to confirm your /r/CalPoly flair.\n"
+    msg += "Confirmation number: " + str(confirm_num)
+    args = (("to", "rCalPolyBot"), ("subject", "Add flair"), ("message", msg))
+    compose = 'http://reddit.com/message/compose/?'
+    return compose + urlencode(args)
+
+
+def save_user(form, confirm_num):
+    """ Given a form object, extract and save the user in the database """
+    user = form.save(commit=False)
+    user.confirmed = False
+    user.confirm_num = confirm_num
+    user.save()
 
 
 def generate_confirm_num():
