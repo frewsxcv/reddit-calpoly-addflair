36bd41463677f5cb358cfa2f058facf3ba36640f
Corey Farwell <coreyf@rwell.org>
2011-08-23 18:06:47 -0700

    added appropriate captcha fields/models

--- addFlair/forms.py
+++ addFlair/forms.py
@@ -1,9 +1,11 @@
 import calpoly
 
 from django import forms
+from captchaform import CaptchaField
 
 
 class RegistrationForm(forms.Form):
     username = forms.CharField(max_length=20)
     year = forms.ChoiceField(choices=calpoly.years())
     major = forms.ChoiceField(choices=calpoly.majors())
+    captcha_field = CaptchaField()
--- addFlair/models.py
+++ addFlair/models.py
@@ -7,3 +7,4 @@ class User(models.Model):
     username = models.CharField(max_length=20)
     year = models.PositiveSmallIntegerField(choices=calpoly.years())
     major = models.CharField(max_length=50, choices=calpoly.majors())
+    confirmed = models.BooleanField()
