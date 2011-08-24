5b10b2df0239f5846ef564a50920cc7f18737bb3
Corey Farwell <coreyf@rwell.org>
2011-08-23 23:33:06 -0700

    added confirmation number field

--- add_flair/models.py
+++ add_flair/models.py
@@ -7,4 +7,5 @@ class User(models.Model):
     username = models.CharField(max_length=20)
     year = models.PositiveSmallIntegerField(choices=calpoly.years())
     major = models.CharField(max_length=50, choices=calpoly.majors())
+    confirm_num = models.IntegerField()
     confirmed = models.BooleanField()
