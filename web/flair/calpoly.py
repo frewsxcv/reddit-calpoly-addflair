d8c4dee3e05bc384c6da9dbf578f2a8faa797132
Corey Farwell <coreyf@rwell.org>
2011-09-12 02:04:19 -0700

    completely restructured

--- web/flair/calpoly.py
+++ web/flair/calpoly.py
@@ -2,7 +2,8 @@ from datetime import date
 
 
 colleges = {
-    'College of Agriculture': {
+    # College of Agriculture
+    'CAFES': {
         'AGB': 'Agribusiness',
         'AGSC': 'Agricultural Science',
         'ASM': 'Agricultural Systems Management',
@@ -23,20 +24,23 @@ colleges = {
         'SOIL': 'Soil Science',
         'WVIT': 'Wine & Viticulture',
     },
-    'College of Architecture & Environmental Design': {
+    # College of Architecture & Environmental Design
+    'CAED': {
         'ARCE': 'Architectural Engineering',
         'ARCH': 'Architecture ',
         'CRP': 'City and Regional Planning',
         'CM': 'Construction Management',
         'LA': 'Landscape Architecture',
     },
-    'Orfalea College of Business': {
+    # Orfalea College of Business
+    'COB': {
         'BT': 'Business & Technology',
         'BUS': 'Business Administration',
         'ECON': 'Economics',
         'IT': 'Industrial Technology',
     },
-    'College of Engineering': {
+    # College of Engineering
+    'CENG': {
         'AERO': 'Aerospace Engineering',
         'CE': 'Civil Engineering',
         'CEENVE': 'Civil and Environmental Engineering',
@@ -52,7 +56,8 @@ colleges = {
         'ME': 'Mechanical Engineering',
         'SE': 'Software Engineering',
     },
-    'College of Liberal Arts': {
+    # College of Liberal Arts
+    'CLA': {
         'ANTGEOG': 'Anthropology and Geography',
         'CD': 'Child Development',
         'COMS': 'Communication Studies',
@@ -69,7 +74,8 @@ colleges = {
         'SOCS': 'Social Sciences',
         'SOC': 'Sociology',
     },
-    'College of Science and Mathematics': {
+    # College of Science and Mathematics
+    'COSAM': {
         'BCHM': 'Biochemistry',
         'BIO': 'Biological Sciences',
         'CHEM': 'Chemistry',
@@ -86,7 +92,7 @@ colleges = {
 
 def majors():
     """
-    Returns a list of tuples of Cal Poly majors
+    Returns a dictionary of Cal Poly majors
     """
     majors = {}
     for college in colleges.values():
@@ -100,7 +106,6 @@ def years():
     Generates a list of possible Cal Poly graduation years
     """
     years = range(1901, date.today().year + 7)
-    years = zip(years, years)
     years.reverse()
     return years
 
