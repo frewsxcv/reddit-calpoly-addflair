39e9ad6c38dd7f67fb1bf7c6fe4c3e52a87aec57
Corey Farwell <coreyf@rwell.org>
2012-12-25 00:55:18 +0000

    remove old files

--- reddit-calpoly-addflair/register/admin.py
+++
@@ -1,7 +0,0 @@
-from register.models import User
-from django.contrib import admin
-
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'major', 'year', 'confirmed')
-
-admin.site.register(User, UserAdmin)
